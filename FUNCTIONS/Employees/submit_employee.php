5b8e9fd69323b1ef6d714da437b1da46ad6f14c0
Rafael Pascual <rafael.pascual@team.acquireap.com>
2016-06-02 13:23:46 +0800

    Added incomplete PHP Mailer

--- FUNCTIONS/Employees/submit_employee.php
+++ FUNCTIONS/Employees/submit_employee.php
@@ -1,6 +1,7 @@
 <?php
 require_once('../connect.php');
 require_once('../../CLASSES/Employees.php');
+require_once('../../CLASSES/PHPMailerAutoload.php');
 
 
 $class = new Employees(
@@ -21,14 +22,48 @@ $class = new Employees(
 
 $data = $class-> create();
 
-
-
 setcookie('commented', 'commented', time()+43200000, '/');
 
 header("HTTP/1.0 500 Internal Server Error");
 if($data['status']==true){
 	header("HTTP/1.0 200 OK");
- }                  
+
+        $mail = new PHPMailer;
+
+        //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'rpascual.chrs@gmail.com';          // SMTP username
+        $mail->Password = '1Loveyou';                         // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
+
+        $mail->setFrom('welcome@chrsglobal.com', 'Mailer');
+        $mail->addAddress($_POST['email_address'], $_POST['first_name']." ".$_POST['last_name']);     // Add a recipient
+        //$mail->addReplyTo('info@example.com', 'Information');
+        $mail->addBCC('rpascual.chrs@gmail.com');
+
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        $mail->isHTML(true);                                  // Set email format to HTML
+
+        $mail->Subject = 'Welcome to CHRS, Inc.';
+        $mail->Body    = 'Hi ' . $_POST['first_name'] . ",";
+        $mail->Body    = '<br /><br />';
+        $mail->Body    = '<b>Welcome!</b>';
+        $mail->Body    = '<br /><br />';
+        $mail->Body    = 'Your CHRS account has been created. Please go to <a href="http://192.168.1.10/oneteam/">http://192.168.1.10/oneteam/</a>.';
+
+        if(!$mail->send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+            header("HTTP/1.0 500 Internal Server Error");
+        } else {
+            header('Content-Type: application/json');
+            print(json_encode($data));
+       }
+}                  
 
 header('Content-Type: application/json');
 print(json_encode($data));
