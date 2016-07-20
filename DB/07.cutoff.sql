3f5287cf954c5d900bd38b1a8d9fb133c753629c
Rafael Pascual <rpascual.chrs@gmail.com>
2016-07-20 14:48:50 +0800

    Added cutoff table

---
+++ DB/07.cutoff.sql
@@ -0,0 +1,65 @@
+begin;
+create table cutoff_types (
+	pk serial primary key,
+	type text not null,
+	archived boolean default false
+);
+alter table cutoff_types owner to chrs;
+
+create table cutoff_dates (
+	cutoff_types_pk int references cutoff_types(pk),
+	dates jsonb not null,
+	archived boolean default false
+);
+alter table cutoff_dates owner to chrs;
+
+insert into cutoff_types
+(
+	type
+)
+values
+(
+	'Monthly'
+),
+(
+	'Bi-Monthly'
+);
+
+insert into cutoff_dates
+(
+	cutoff_types_pk,
+	dates
+)
+values
+(
+	2,
+	'{
+		"cutoff" : [
+					 {
+						"from" : 1,
+						"to" : 15
+					},
+					{
+						"from" : 16,
+						"to" : 30
+					}
+				]
+	}'
+);
+
+/*
+values
+(
+	1,
+	'{
+		"cutoff" : [
+					 {
+						"from" : 1,
+						"to" : 30
+					}
+				]
+	}'
+)
+*/
+
+commit;
