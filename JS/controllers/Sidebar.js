93aede894c284632f26722be9ca190aa960822c5
ClintAnthony Decena <iamclintanthony@gmail.com>
2016-08-09 18:19:17 +0800

    animate toggle

--- JS/controllers/Sidebar.js
+++ JS/controllers/Sidebar.js
@@ -6,16 +6,39 @@ app.controller('Sidebar', function(
     $scope.switcher = {};
     $scope.switcher.main = "";
 
+
+
     $scope.toggle_switcher = function(){
         if($scope.switcher.main == ""){
             $scope.switcher.main = "open";
             $scope.switcher.content = true;
-        }
+            $scope.stop = true;
+       
+           
+         }
         else {
             $scope.switcher.main = "";   
             $scope.switcher.content = false;
-        }
+            $scope.stop = false;
+            
+         }
+
+
     }
 
+    $scope.getStop = function(){
+        if($scope.stop == true)
+        {
+          return '0s';
+        }
+        else
+        {
+
+          return '1s';
+        }
+
+
+
+    }
     
 });
