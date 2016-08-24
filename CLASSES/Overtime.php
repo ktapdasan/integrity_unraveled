3b9e4b01e7580fc1e346cf3ab3b31b5f69382e82
Patrick M. Chavez <pchavez.chrs@gmail.com>
2016-08-24 10:01:59 +0800

    merged

--- CLASSES/Overtime.php
+++ CLASSES/Overtime.php
@@ -46,6 +46,126 @@ EOT;
         return ClassParent::get($sql);
     }
 
+    public function overtime($data){
+
+        $where = "";
+        if($this->employees_pk){
+            $where .= "and employees_pk = ".$this->employees_pk;
+        }
+        $datefrom = $data['datefrom'] ;
+        $dateto = $data['dateto'];
+        $sql = <<<EOT
+                select
+                    pk, 
+                    employees_pk,
+                    (select first_name||' '||last_name from employees where pk = employees_pk) as name,
+                    time_to :: time as timeto,
+                    time_from :: time as timefrom,
+                    date_created::date as datecreated,
+                    (select status from overtime_status where pk = overtime_pk order by date_created desc limit 1) as status
+                from overtime
+                where date_created::date between '8-16-2016' and '8-26-2016'
+                and archived = 'f'
+                $where
+                ;
+EOT;
+
+        return ClassParent::get($sql);
+
+    }
+
+    public function cancel($info){
+
+        $overtime_pk=$info['overtime_pk'];
+        $sql = 'begin;';
+        $sql .= <<<EOT
+                UPDATE overtime_status
+                set archived = true
+                where overtime_pk = $overtime_pk
+                ;
+EOT;
+         $sql .= <<<EOT
+                UPDATE overtime
+                set archived = true
+                where pk = $overtime_pk
+                ;
+EOT;
+
+        $sql .= "commit;";
+        return ClassParent::update($sql);
+    }
+
+    public function insert_overtime($info){
+        foreach($info as $k=>$v){
+            $info[$k] = pg_escape_string(trim(strip_tags($v)));
+        }
+    
+        
+        $employees_pk=$info['employees_pk'];
+        $overtime_pk=$info['overtime_pk'];
+        $created_by=$info['created_by'];
+        $status=$info['status'];
+        $remarks=strtoupper($info['remarks']);
+    
+        $sql = 'begin;';
+
+        $sql .= <<<EOT
+                        update overtime_status set
+                        
+                            remarks
+                        =
+                            '$remarks'
+                        
+                       where overtime_pk = $overtime_pk
+                        ;
+EOT;
+        if($status=='Approved'){
+            $sql .= <<<EOT
+
+                insert into overtime_status
+                (
+                    overtime_pk,
+                    created_by,
+                    status,
+                    remarks           
+                )
+                values
+                (    
+                    $overtime_pk,
+                    $created_by,
+                    'Approved',
+                    'APPROVED'
+                )
+                ;
+EOT;
+        }else{
+
+            $sql .= <<<EOT
+
+                insert into overtime_status
+                (
+                    overtime_pk,
+                    created_by,
+                    status,
+                    remarks           
+                )
+                values
+                (    
+                    $overtime_pk,
+                    $created_by,
+                    'Disapproved',
+                    '$remarks'
+                )
+                ;
+EOT;
+        }
+    
+
+    $sql .= "commit;";
+        return ClassParent::insert($sql);
+
+    }
+
     public function insert($data){
         $remarks = pg_escape_string(strip_tags(trim($data['remarks'])));
 
