bd77864aad2c82b272efd2d6d378a737cd634ed0
ktapdasan <ktapdasan.chrs@gmail.com>
2016-09-05 15:26:03 +0800

    Added Capital on First Letter

---
+++ JS/directives/capitalfirst.js
@@ -0,0 +1,19 @@
+app.directive('capitalizeFirst', function(uppercaseFilter, $parse) {
+   return {
+     require: 'ngModel',
+     link: function(scope, element, attrs, modelCtrl) {
+        var capitalize = function(inputValue) {
+           var capitalized = inputValue.charAt(0).toUpperCase() +
+               inputValue.substring(1);
+           if(capitalized !== inputValue) {
+              modelCtrl.$setViewValue(capitalized);
+              modelCtrl.$render();
+            }         
+            return capitalized;
+         }
+         var model = $parse(attrs.ngModel);
+         modelCtrl.$parsers.push(capitalize);
+         capitalize(model(scope));
+     }
+   };
+});
