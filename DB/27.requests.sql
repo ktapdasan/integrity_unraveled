9897c220e6061d3884181e53a94b1dfb26ab18a1
Rafael Pascual <rpascual.chrs@gmail.com>
2016-10-07 13:46:56 +0800

    Updated requests db

--- DB/27.requests.sql
+++ DB/27.requests.sql
@@ -6,20 +6,28 @@ create table request_type (
 );
 alter table request_type owner to chrs;
 
-create table requests_handlers (
-	employees_pk int references employees(pk),
-	created_by int references employees(pk),
-	date_created timestamptz default now(),
-	constraint employees_pk_unique unique(employees_pk)
-);
-alter table requests_handlers owner to chrs;
+drop table requests_handlers;
+-- create table requests_handlers (
+-- 	employees_pk int references employees(pk),
+-- 	created_by int references employees(pk),
+-- 	date_created timestamptz default now(),
+-- 	constraint employees_pk_unique unique(employees_pk)
+-- );
+-- alter table requests_handlers owner to chrs;
 
+drop table requests;
 create table requests (
 	pk serial primary key,
 	request_type_pk int references request_type(pk),
-	reason text not null,
 	created_by int references employees(pk),
 	date_created timestamptz default now(),
 	archived boolean default false
+);
+alter table requests owner to chrs;
+
+create table requests_status (
+	requests_pk int references requests(pk),
+	status text default 'Pending',
+	remarks text
 );
 alter table requests owner to chrs;
