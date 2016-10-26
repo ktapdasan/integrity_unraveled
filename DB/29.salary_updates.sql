e24ea7571f198bb10edf5af180b0d9093fd76aff
Rafael Pascual <rpascual.chrs@gmail.com>
2016-10-26 14:11:20 +0800

    Added salary updates

---
+++ DB/29.salary_updates.sql
@@ -0,0 +1,51 @@
+create table rate_types (
+	pk serial primary key,
+	type text not null,
+	archived boolean default false
+);
+alter table rate_types owner to chrs;
+
+create table pay_period (
+	pk serial primary key,
+	period text not null,
+	archived boolean default false
+);
+alter table rate_types owner to chrs;
+
+insert into rate_types
+(
+	type
+)
+values
+(
+	'Hourly'
+),
+(
+	'Daily'
+),
+(
+	'Monthly'
+)
+;
+
+insert into pay_period
+(
+	period
+)
+values
+(
+	'Daily'
+),
+(
+	'Weekly'
+),
+(
+	'Semi-Monthly'
+),
+(
+	'Monthly'
+),
+(
+	'Annually'
+)
+;
